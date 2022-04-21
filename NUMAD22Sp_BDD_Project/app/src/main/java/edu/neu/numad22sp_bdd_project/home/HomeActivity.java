b2790f3c6d4f4d4efb018f1ef30a89d2d06fb0ab
scenery <wanglinsung@gmail.com>
2022-04-20 21:51:34 -0400

    add sidebar

--- NUMAD22Sp_BDD_Project/app/src/main/java/edu/neu/numad22sp_bdd_project/home/HomeActivity.java
+++ NUMAD22Sp_BDD_Project/app/src/main/java/edu/neu/numad22sp_bdd_project/home/HomeActivity.java
@@ -1,14 +1,83 @@
 package edu.neu.numad22sp_bdd_project.home;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.DialogFragment;
 import edu.neu.numad22sp_bdd_project.R;
-import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    protected DrawerLayout draw;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        draw = findViewById(R.id.drawer_layout);
+        NavigationView navi = findViewById(R.id.nav_view);
+        navi.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, draw, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        draw.addDrawerListener(toggle);
+        toggle.syncState();
+
+        final FrameLayout frag = findViewById(R.id.frag_container);
+
+
+        getLayoutInflater().inflate(R.layout.activity_background, frag);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+
+            case R.id.nav_home:
+                Intent i_home = new Intent(this, HomeActivity.class);
+                startActivity(i_home);
+                draw.closeDrawer(GravityCompat.START);
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        //close navigation bar before closing app
+        if(draw.isDrawerOpen(GravityCompat.START)){
+            draw.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
