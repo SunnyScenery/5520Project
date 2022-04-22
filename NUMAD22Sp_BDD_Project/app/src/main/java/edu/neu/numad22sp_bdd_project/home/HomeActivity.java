97b69636e0be9250887aafd299bf686a49d2c869
kimrine <347642700@qq.com>
2022-04-21 23:10:51 -0400

    add home daily quote feature and return to phone home after click back button when in app Home page

--- NUMAD22Sp_BDD_Project/app/src/main/java/edu/neu/numad22sp_bdd_project/home/HomeActivity.java
+++ NUMAD22Sp_BDD_Project/app/src/main/java/edu/neu/numad22sp_bdd_project/home/HomeActivity.java
@@ -1,83 +1,38 @@
 package edu.neu.numad22sp_bdd_project.home;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.DialogFragment;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+
 import edu.neu.numad22sp_bdd_project.R;
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
+import android.media.Image;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
-import androidx.appcompat.widget.Toolbar;
+import java.util.Random;
 
-import com.google.android.material.navigation.NavigationView;
+public class HomeActivity extends MainActivity {
 
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-    protected DrawerLayout draw;
-    ImageView imageView;
+    private ImageView homewall;
+    private DailySentence dailySentence;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.imageView);
+        final FrameLayout contentFrameLayout = findViewById(R.id.frag_container);
+        dailySentence = new DailySentence();
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //get random daily wallpaper
+        Random num = new Random();
+        int id = num.nextInt(60);
 
-        draw = findViewById(R.id.drawer_layout);
-        NavigationView navi = findViewById(R.id.nav_view);
-        navi.setNavigationItemSelectedListener(this);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, draw, toolbar, R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close);
-        draw.addDrawerListener(toggle);
-        toggle.syncState();
-
-        final FrameLayout frag = findViewById(R.id.frag_container);
-
-
-        getLayoutInflater().inflate(R.layout.activity_background, frag);
+        //set it to main activity
+        getLayoutInflater().inflate(R.layout.wallpaperlayout, contentFrameLayout);
+        homewall = findViewById(R.id.wallpaperView);
+        homewall.setImageResource(dailySentence.getImage(id));
+//        }
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        switch(item.getItemId()){
-
-            case R.id.nav_home:
-                Intent i_home = new Intent(this, HomeActivity.class);
-                startActivity(i_home);
-                draw.closeDrawer(GravityCompat.START);
-                break;
-        }
-
-        return true;
-    }
-
-    @Override
-    public void onBackPressed() {
-        //close navigation bar before closing app
-        if(draw.isDrawerOpen(GravityCompat.START)){
-            draw.closeDrawer(GravityCompat.START);
-        }
-        else {
-            super.onBackPressed();
-        }
-    }
 }
